a7bd4905b0eba4fb7cb3d59d0da5a0b78bb18655
ramakrishnajunnu <ramakrishnajunnu@gmail.com>
2023-06-10 21:02:50 +0530

    playbook

---
+++ 09.provisioner/main.tf
@@ -0,0 +1,32 @@
+######create ec2 instance terraform######
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.example.id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-0041cea51ab9bf7eb"]
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+
+provisioner "remote-exec" {
+
+  connection {
+    type     = "ssh"
+    user     = "centos"
+    password = "DevOps321"
+    host     = self.public_ip
+  }
+  inline = [
+    "sudo labauto ansible",
+    "ansible-pull -i localhost, -U https://github.com/jvrkrishna/roboshopp-ansible roboshop.yml -e role_name=frontend",
+  ]
+}
+
+############ aws ami id datasource terraform #############
+data "aws_ami" "example" {
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+  owners = ["973714476881"]
+}
