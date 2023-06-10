b87f73511bd177e423ac58739002384c24265358
ramakrishnajunnu <ramakrishnajunnu@gmail.com>
2023-06-10 10:39:07 +0530

    playbook

--- 05.Datasources/data.tf
+++ 05.Datasources/data.tf
@@ -24,7 +24,6 @@ output "sgid" {
 
 ############ aws ami id datasource terraform #############
 data "aws_ami" "example" {
-  executable_users = ["self"]
   most_recent      = true
   name_regex       = "Centos-8-DevOps-Practice"
   owners = ["973714476881"]
