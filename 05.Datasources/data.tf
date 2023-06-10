9dd499552b17aa61bfe74c7b503add9980c08619
ramakrishnajunnu <ramakrishnajunnu@gmail.com>
2023-06-10 10:41:08 +0530

    playbook

--- 05.Datasources/data.tf
+++ 05.Datasources/data.tf
@@ -10,7 +10,7 @@ data "aws_ec2_spot_price" "example" {
 }
 
 output "price" {
-  value = data.aws_ec2_spot_price.example.spot_price
+  value = data.aws_ec2_spot_price.example.id
 }
 
 ###### aws security group datasource terraform #######
