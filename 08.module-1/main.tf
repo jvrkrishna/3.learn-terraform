1582fcc99b6fc39ec5b78a1dd462697f41d14541
ramakrishnajunnu <ramakrishnajunnu@gmail.com>
2023-06-10 17:02:18 +0530

    playbook

--- 08.module-1/main.tf
+++ 08.module-1/main.tf
@@ -1,19 +1,19 @@
 module "sample1" {
   source = "./ec2"
-  name= "sample1"   ##### for this add variable "name" {} in main.tf in last and change the sample to var.sample
+  name= "sample3"   ##### for this add variable "name" {} in main.tf in last and change the sample to var.sample
 }
 
 module "sample2" {
   source = "./ec2"
-  name = "sample2"
+  name = "sample4"
 }
 
-#output "pubilc_ip_sample1" {
+#output "pubilc_ip_sample3" {
  # description = "Public IP address of the EC2 instance"
  # value = module.sample1.public_ip
 #}
 
-#output "pubilc_ip_sample2" {
+#output "pubilc_ip_sample4" {
 #  description = "Public IP address of the EC2 instance"
 #  value = module.sample2.public_ip
 #}
