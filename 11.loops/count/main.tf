72164b2612aef06569bfea11f353c423621c40ff
ramakrishnajunnu <ramakrishnajunnu@gmail.com>
2023-06-12 13:19:18 +0530

    playbook

---
+++ 11.loops/count/main.tf
@@ -0,0 +1,22 @@
+############ create ec2 instance in terraform ###############
+resource "aws_instance" "web" {
+  count = 3
+  ami           = data.aws_ami.example.id
+  instance_type = "t3.small"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+############## create aws_ami in terraform ##############
+data "aws_ami" "example" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
+}
+
+
+variable "instances" {
+  value = ["frontend", "mongodb", "catalogue"]
+}
---
+++ 12.functions/main.tf
@@ -0,0 +1,8 @@
+variable "class" {
+  default = "devops"
+}
+
+###### print the same in Capital Letters ######
+output "calss" {
+  value = var.class
+}
