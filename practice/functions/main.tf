f23b57e73a8fd0c4132163b7f96458530139d112
ramakrishnajunnu <ramakrishnajunnu@gmail.com>
2023-06-12 19:49:30 +0530

    playbook

--- practice/functions/main.tf
+++ practice/functions/main.tf
@@ -33,5 +33,5 @@ variable "fruit_map" {
 }
 
 output "fruit_map" {
-  value = lookup(lookup(var.fruit_map, "fruit3", null ), "color", null)
+  value = lookup(lookup(var.fruit_map, "fruit3", null ), color, null)
 }
