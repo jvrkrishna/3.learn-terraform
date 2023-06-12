9ad237080717ca460297e2c127df586dd151fb40
ramakrishnajunnu <ramakrishnajunnu@gmail.com>
2023-06-12 15:37:38 +0530

    playbook

---
+++ 11.loops/for-each/main.tf
@@ -0,0 +1,34 @@
+############ create ec2 instance in terraform ###############
+resource "aws_instance" "web" {
+  for_each = var.instances
+  ami           = data.aws_ami.example.id
+  instance_type = lookup(each.value, "instance_type", "t3.small" )
+
+  tags = {
+    Name = each.key
+  }
+}
+
+############## create aws_ami in terraform ##############
+data "aws_ami" "example" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
+}
+
+
+variable "instances" {
+  default = {
+    frontend = {
+      name = "frontend"
+    }
+    catalogue = {
+      name = "catalogue"
+      instance_type= "t3.small"
+    }
+    cart = {
+      name = "cart"
+      instance_type= "t3.small"
+    }
+  }
+}
