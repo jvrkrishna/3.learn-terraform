5c662135529f59623e9cff76293b5bf2f0e53d99
ramakrishnajunnu <ramakrishnajunnu@gmail.com>
2023-06-12 13:40:34 +0530

    playbook

--- 12.functions/main.tf
+++ 12.functions/main.tf
@@ -1,3 +1,4 @@
+########### plain type variable declaration ##########
 variable "class" {
   default = "devops"
 }
@@ -5,4 +6,33 @@ variable "class" {
 ###### print the same in Capital Letters ######
 output "class" {
   value = upper(var.class)
+}
+
+######## List type declaration ###########
+variable "courses" {
+  default = ["Devops", "Jenkins"]
+}
+
+###### print the count of list ##########
+output "courses_list" {
+  value = length(var.courses)
+}
+
+######### map type declaration #########
+variable "course" {
+  default = {
+    course1 = {
+      name = "Devops"
+      time = "90days"
+    }
+    course2 = {
+      name = "AWS"
+      timings = ["4h", "3hr"]
+    }
+  }
+}
+
+######## To print the map output ###########
+output "couses" {
+  value = var.course["course1"]["name"]
 }
