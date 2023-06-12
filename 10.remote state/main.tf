37dbcbc8224a9beea1ec871419feb4cbb36a1cf0
ramakrishnajunnu <ramakrishnajunnu@gmail.com>
2023-06-12 09:30:53 +0530

    playbook

---
+++ 10.remote state/main.tf
@@ -0,0 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket = "mybucket"
+    key    = "path/to/my/key"
+    region = "us-east-1"
+  }
+}
+
+#### storing some output ####
+output "demo"{
+  value = "Hello world"
+}
