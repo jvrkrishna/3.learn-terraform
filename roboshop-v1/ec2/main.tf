cfa02c2c4e571217d578b90fb93c5cb511225b05
ramakrishnajunnu <ramakrishnajunnu@gmail.com>
2023-06-13 15:09:33 +0530

    playbook

--- roboshop-v1/ec2/main.tf
+++ roboshop-v1/ec2/main.tf
@@ -8,18 +8,19 @@ resource "aws_instance" "web" {
     Name = var.name
   }
 }
-  provisioner "remote-exec" {
-    connection {
-      type     = "ssh"
-      user     = "centos"
-      password = "DevOps321"
-      host     = aws_instance.web.public_ip
-    }
-    inline = [
+provisioner "remote-exec" {
+
+  connection {
+    type     = "ssh"
+    user     = "centos"
+    password = "DevOps321"
+    host     = aws_instance.web.public_ip
+  }
+   inline = [
       "sudo labauto ansible",
       "ansible-pull -i localhost, -U https://github.com/jvrkrishna/roboshopp-ansible roboshop.yml -e role_name=${var.name}"
     ]
-  }
+}
 
 
 resource "aws_route53_record" "www" {
