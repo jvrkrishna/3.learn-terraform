8c5b95e23a7663c1cdb2b4cd917ea63b35bbcf4e
ramakrishnajunnu <ramakrishnajunnu@gmail.com>
2023-06-13 15:06:13 +0530

    playbook

--- roboshop-v1/ec2/main.tf
+++ roboshop-v1/ec2/main.tf
@@ -1,25 +1,27 @@
 ###### create ec2 instance terraform ######
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.example.id
-  instance_type = "t3.small"
+  ami                    = data.aws_ami.example.id
+  instance_type          = "t3.small"
   vpc_security_group_ids = [aws_security_group.sg.id]
 
   tags = {
     Name = var.name
   }
 }
-provisioner "remote-exec" {
-  connection {
-    type     = "ssh"
-    user     = "centos"
-    password = "DevOps321"
-    host     = aws_instance.web.public_ip
+  provisioner "remote-exec" {
+    connection {
+      type     = "ssh"
+      user     = "centos"
+      password = "DevOps321"
+      host     = aws_instance.web.public_ip
+    }
+    inline = [
+      "sudo labauto ansible",
+      "ansible-pull -i localhost, -U https://github.com/jvrkrishna/roboshopp-ansible roboshop.yml -e role_name=${var.name}"
+    ]
   }
-  inline = [
-    "sudo labauto ansible",
-    "ansible-pull -i localhost, -U https://github.com/jvrkrishna/roboshopp-ansible roboshop.yml -e role_name=${var.name}"
-  ]
-}
+
+
 resource "aws_route53_record" "www" {
   zone_id = "Z0858447245XTBTK7DY06"
   name    = "${var.name}-dev"
