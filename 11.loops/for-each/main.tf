5a78c68e0fcf71d73f421956b10f1fe4c1ae5d3a
Rama Krishna <jvrkrishnaa@gmail.com>
2023-12-08 14:37:07 +0530

    s

--- 11.loops/for-each/main.tf
+++ 11.loops/for-each/main.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "web" {
   for_each = var.instances
   ami           = data.aws_ami.example.id
-  instance_type = lookup(each.value, "instance_type", "t3.small" )
+  instance_type = lookup(each.value, "instance_type", "t2.micro" )
 
   tags = {
     Name = each.key
